2417241c52be4c2f7fab3e9e4d7f9e02bca3bf06
WebPro <webpro255@gmail.com>
2024-12-14 23:09:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function generatePayload() {
+    const tag = document.getElementById('tag').value;
+    const event = document.getElementById('event').value;
+    const payload = document.getElementById('payload').value;
+
+    const generatedPayload = `<${tag} ${event}="${payload}">`;
+    document.getElementById('output').value = generatedPayload;
+}
 
